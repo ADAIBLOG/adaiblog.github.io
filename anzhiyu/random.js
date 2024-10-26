3788dc65c06c771eea8d202f47fd779b25473ba6
adaiblog <adaigeek@189.cn>
2024-10-26 23:14:04 +0800

    Site updated: 2024-10-26 23:14:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/26/Steam展柜/"];function toRandomPost(){
+var posts=["2024/10/26/Steam展柜/","2022/03/01/TeamSpeak/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
