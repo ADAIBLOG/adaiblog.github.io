61407b0de3cb8e827e5c56a4c74f203b55e0d32c
adaiblog <adaigeek@189.cn>
2024-11-20 23:16:57 +0800

    Site updated: 2024-11-20 23:16:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/30/Debian系统/","2024/11/01/Docker/","2024/11/13/Github搭建个人博客/","2024/10/26/Steam展柜/","2024/10/31/Windows/","2022/03/01/TeamSpeak/","2024/10/30/cs2/","2024/11/08/mumble/","2024/11/12/网站推荐/","2024/10/27/应用软件/","2024/11/17/cscfg/"];function toRandomPost(){
+var posts=["2024/10/30/Debian系统/","2024/11/01/Docker/","2024/11/13/Github搭建个人博客/","2024/10/26/Steam展柜/","2024/10/31/Windows/","2022/03/01/TeamSpeak/","2024/10/30/cs2/","2024/11/08/mumble/","2024/11/12/网站推荐/","2024/10/27/应用软件/","2024/11/17/cscfg/","2024/11/20/软件导航（云盘版）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
