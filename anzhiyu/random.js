7ebdf6285e7e8fab01742207ca9fe98957d9b30a
adaiblog <adaigeek@189.cn>
2024-11-25 03:18:37 +0800

    Site updated: 2024-11-25 03:18:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/apex.html","posts/app.html","posts/App2.html","posts/debian.html","posts/steam.html","posts/blog.html","posts/docker.html","posts/steam2.html","posts/windwos.html","posts/cs2.html","posts/teamspeak.html","posts/cs2cfg.html","posts/mumble.html","posts/web.html"];function toRandomPost(){
+var posts=["posts/App2.html","posts/app.html","posts/debian.html","posts/docker.html","posts/apex.html","posts/blog.html","posts/steam2.html","posts/steam.html","posts/teamspeak.html","posts/windwos.html","posts/cs2.html","posts/mumble.html","posts/cs2cfg.html","posts/web.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
