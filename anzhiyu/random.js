151d3ecaeea2144f336d2bb7931674d29e4c57bb
adaiblog <adaigeek@189.cn>
2024-11-30 15:54:25 +0800

    Site updated: 2024-11-30 15:54:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/apex.html","posts/docker.html","posts/App2.html","posts/app.html","posts/debian.html","posts/github.html","posts/steam.html","posts/steam2.html","posts/windwos.html","posts/cs2.html","posts/teamspeak.html","posts/cs2cfg.html","posts/web.html","posts/mumble.html","posts/blog.html"];function toRandomPost(){
+var posts=["posts/App2.html","posts/debian.html","posts/apex.html","posts/app.html","posts/github.html","posts/docker.html","posts/steam.html","posts/steam2.html","posts/teamspeak.html","posts/windwos.html","posts/cs2.html","posts/cs2cfg.html","posts/mumble.html","posts/blog.html","posts/web.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
